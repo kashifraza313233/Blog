19ae807abe7b94ffd8e490e1e2314336d6f4ef4c
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-06-01 01:09:35 +0500

    Initial

--- src/Bloggie/Blog_WebAPP/Models/Domain/BlogPost.cs
+++ src/Bloggie/Blog_WebAPP/Models/Domain/BlogPost.cs
@@ -3,15 +3,16 @@
     public class BlogPost
     {
         public Guid ID { get; set; }
-        public string? Heading { get; set; }
-        public string? PageTitle { get; set; }
-        public string? Content { get; set; }
-        public string? ShortDescription { get; set; }
-        public string? ImageUrl { get; set; }
-        public string? UrlHandler { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public string UrlHandler { get; set; }
         public DateTime PublisDate { get; set; }
-        public string? Author{ get; set; }
-        public bool? Visible{ get; set; }
+        public string Author{ get; set; }
+        public bool Visible{ get; set; }
+        public ICollection<Tag> Tags { get; set; }
 
     }
 }
---
+++ src/Bloggie/Blog_WebAPP/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog_WebAPP.Models.Domain
+{
+    public class Tag
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set;}
+    }
+}
