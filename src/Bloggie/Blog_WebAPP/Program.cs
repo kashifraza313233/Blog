e473d243f8de339cacaa64138aa957f6f69f14a9
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-06-06 21:40:20 +0500

    Initial

--- src/Bloggie/Blog_WebAPP/Program.cs
+++ src/Bloggie/Blog_WebAPP/Program.cs
@@ -1,10 +1,15 @@
+using Blog_WebAPP.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<Db_ContextBlog>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("Constring")));
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
