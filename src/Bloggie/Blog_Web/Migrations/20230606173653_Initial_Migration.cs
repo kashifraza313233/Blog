1d09491f0bd2b96287e8fdff664afa652e4ae258
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-06-07 00:34:00 +0500

    intial

---
+++ src/Bloggie/Blog_Web/Migrations/20230606173653_Initial_Migration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blob_Posts",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Heading = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PageTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlHandler = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublisDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visible = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blob_Posts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blog_Tags",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog_Tags", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogPostsTags",
+                columns: table => new
+                {
+                    BlogPostID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPostsTags", x => new { x.BlogPostID, x.TagID });
+                    table.ForeignKey(
+                        name: "FK_BlogPostsTags_Blob_Posts_BlogPostID",
+                        column: x => x.BlogPostID,
+                        principalTable: "Blob_Posts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogPostsTags_Blog_Tags_TagID",
+                        column: x => x.TagID,
+                        principalTable: "Blog_Tags",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPostsTags_TagID",
+                table: "BlogPostsTags",
+                column: "TagID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPostsTags");
+
+            migrationBuilder.DropTable(
+                name: "Blob_Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blog_Tags");
+        }
+    }
+}
